2b364b692e889a3112a594c444626c397e4c7892
Dave Xiong <hxiong@consoleconnect.com>
2024-10-25 22:26:44 +1000

    feat(sdk): add endpoints for organization's connection mgmt (#14)

---
+++ vortex-java-sdk/vortex-java-sdk-core/src/main/java/com/consoleconnect/vortex/core/config/WebFluxConfiguration.java
@@ -0,0 +1,55 @@
+package com.consoleconnect.vortex.core.config;
+
+import com.consoleconnect.vortex.core.exception.VortexExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+@Configuration
+@AutoConfigureBefore(WebFluxAutoConfiguration.class)
+public class WebFluxConfiguration implements WebFluxConfigurer {
+
+  /** 500KB */
+  private static final int BYTE_COUNT = 1024 * 1024 * 10;
+
+  @Bean
+  public WebProperties.Resources resources() {
+    return new WebProperties.Resources();
+  }
+
+  @Bean
+  @Order(-1)
+  public ErrorWebExceptionHandler errorWebExceptionHandler(
+      ErrorAttributes errorAttributes,
+      WebProperties webProperties,
+      ObjectProvider<ViewResolver> viewResolvers,
+      ServerCodecConfigurer serverCodecConfigurer,
+      ApplicationContext applicationContext) {
+
+    VortexExceptionHandler exceptionHandler =
+        new VortexExceptionHandler(
+            errorAttributes,
+            webProperties.getResources(),
+            serverCodecConfigurer,
+            applicationContext);
+    exceptionHandler.setViewResolvers(viewResolvers.orderedStream().toList());
+    exceptionHandler.setMessageWriters(serverCodecConfigurer.getWriters());
+    exceptionHandler.setMessageReaders(serverCodecConfigurer.getReaders());
+    return exceptionHandler;
+  }
+
+  @Override
+  public void configureHttpMessageCodecs(ServerCodecConfigurer configure) {
+    configure.defaultCodecs().maxInMemorySize(BYTE_COUNT);
+  }
+}
--- vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/CreateOrganizationDto.java
+++ vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/CreateOrganizationDto.java
@@ -1,9 +1,12 @@
 package com.consoleconnect.vortex.iam.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CreateOrganizationDto {
   private String name;
+
+  @JsonProperty("display_name")
   private String displayName;
 }
