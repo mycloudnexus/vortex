09cbc06c9921efe0bde09c10c146bb9268ec845f
jackworkgit <hongzhang@consoleconnect.com>
2024-10-29 18:28:32 +0800

    handle duplicated code

--- vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/UpdateConnectionDto.java
+++ vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/UpdateConnectionDto.java
@@ -1,10 +1,7 @@
 package com.consoleconnect.vortex.iam.dto;
 
 import com.consoleconnect.vortex.iam.enums.ConnectionStrategryEnum;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.Data;
 
 @Data
@@ -14,27 +11,7 @@ public class UpdateConnectionDto {
 
   private ConnectionStrategryEnum strategy = ConnectionStrategryEnum.SAML;
 
-  private Oidc odic;
+  private OidcConnection odic;
 
   private SamlConnection samlConnection;
-
-  @Data
-  public static class Oidc {
-    @JsonProperty("client_id")
-    private String clientId;
-
-    @JsonProperty("discovery_url")
-    private String discoveryUrl;
-
-    private String scope = "openid profile email";
-
-    public Map<String, Object> toMap() {
-      Map<String, Object> map = new HashMap<>();
-      map.put("client_id", getClientId());
-      map.put("discovery_url", getDiscoveryUrl());
-      map.put("scope", getScope());
-      map.put("type", "front_channel");
-      return map;
-    }
-  }
 }
