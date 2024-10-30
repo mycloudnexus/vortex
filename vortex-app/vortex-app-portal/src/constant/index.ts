5034a233b2292d971b226b6cba316e8fa3c1d721
li yang <lyang@pccwglobal.com>
2024-10-30 13:15:18 +0800

    feat(portal): update api key and add env to ignore file

--- vortex-app/vortex-app-portal/src/constant/index.ts
+++ vortex-app/vortex-app-portal/src/constant/index.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-  API_BASE_URL: process.env.BASE_URL,
+  API_BASE_URL: process.env.API_BASE_URL,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
