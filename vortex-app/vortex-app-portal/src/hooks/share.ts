aa518d945d69fbaf9c9236bad55c5ea6ec7eb94e
li yang <lyang@pccwglobal.com>
2024-10-30 18:03:24 +0800

    feat(portal): add share data hooks

---
+++ vortex-app/vortex-app-portal/src/hooks/share.ts
@@ -0,0 +1,9 @@
+import { useAppStore } from '@/stores/app.store'
+const useInitialData = () => {
+  const { roleList, user, currentAuth0User } = useAppStore()
+
+  return { roleList, user, currentAuth0User }
+
+}
+
+export default useInitialData
