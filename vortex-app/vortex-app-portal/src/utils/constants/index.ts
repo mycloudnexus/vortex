210966fcd3c6643338b5e044e1d164f386c5b9fd
li yang <lyang@pccwglobal.com>
2024-10-30 11:11:38 +0800

    feat(portal): remove file

--- vortex-app/vortex-app-portal/src/utils/constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const ENV = {
-  BASE_URL: process.env.BASE_URL,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
-  AUTH0_MGMT_ORG_ID: process.env.AUTH0_MGMT_ORG_ID,
-  VOTEX_MODULES: process.env.VOTEX_MODULES
-}
