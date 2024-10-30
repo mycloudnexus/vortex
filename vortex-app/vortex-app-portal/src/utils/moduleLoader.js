ff79ffc0197e9673bb079b5b062640d47bb4b1a3
li yang <lyang@pccwglobal.com>
2024-10-30 09:09:44 +0800

    feat(portal): update module key

--- vortex-app/vortex-app-portal/src/utils/moduleLoader.js
+++ vortex-app/vortex-app-portal/src/utils/moduleLoader.js
@@ -2,7 +2,7 @@
 import React from 'react'
 
 function parseModules() {
-  const modules = process.env.CONSOLE_MODULES
+  const modules = process.env.VOTEX_MODULES
   const parsedModules = []
   if (typeof modules === 'string') {
     const urls = modules.split(',')
