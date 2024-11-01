8be4179eb7bc9cb563d0793cf308b9a5d569e747
jackworkgit <hongzhang@consoleconnect.com>
2024-11-01 13:32:46 +0800

    add unit test

--- vortex-java-sdk/vortex-java-sdk-iam/src/test/java/com/consoleconnect/vortex/iam/service/OrganizationServiceTest.java
+++ vortex-java-sdk/vortex-java-sdk-iam/src/test/java/com/consoleconnect/vortex/iam/service/OrganizationServiceTest.java
@@ -229,6 +229,73 @@ class OrganizationServiceTest {
   }
 
   @Test
+  void testUpdateStatusInactiveBranch() throws Auth0Exception {
+    ManagementAPI managementAPI = mock(ManagementAPI.class);
+    doReturn(managementAPI).when(auth0Client).getMgmtClient();
+
+    OrganizationsEntity organizationsEntity = mock(OrganizationsEntity.class);
+    doReturn(organizationsEntity).when(managementAPI).organizations();
+
+    Request<Organization> queryRequest = mock(Request.class);
+    doReturn(queryRequest).when(organizationsEntity).get(anyString());
+
+    Response<Organization> queryResponse = mock(Response.class);
+    doReturn(queryResponse).when(queryRequest).execute();
+
+    Organization queryOrganization = new Organization("test");
+    queryOrganization.setDisplayName("test");
+    OrganizationMetadata metadata = new OrganizationMetadata();
+    metadata.setStatus(OrgStatusEnum.ACTIVE);
+    queryOrganization.setMetadata(
+        JsonToolkit.fromJson(JsonToolkit.toJson(metadata), new TypeReference<>() {}));
+    doReturn(queryOrganization).when(queryResponse).getBody();
+
+    Request<Organization> organizationRequest = mock(Request.class);
+    doReturn(organizationRequest).when(organizationsEntity).update(anyString(), any());
+    Response<Organization> updateResponse =
+        new Response<Organization>() {
+          @Override
+          public Map<String, String> getHeaders() {
+            return Map.of();
+          }
+
+          @Override
+          public Organization getBody() {
+            Organization updateOrganization = new Organization("test");
+            updateOrganization.setDisplayName("test");
+            OrganizationMetadata updateMetadata = new OrganizationMetadata();
+            updateMetadata.setStatus(OrgStatusEnum.INACTIVE);
+            updateOrganization.setMetadata(
+                JsonToolkit.fromJson(JsonToolkit.toJson(updateMetadata), new TypeReference<>() {}));
+            return updateOrganization;
+          }
+
+          @Override
+          public int getStatusCode() {
+            return HttpStatus.OK.value();
+          }
+        };
+    doReturn(updateResponse).when(organizationRequest).execute();
+
+    Request<EnabledConnectionsPage> enabledConnectionsPage = mock(Request.class);
+    doReturn(enabledConnectionsPage).when(organizationsEntity).getConnections(anyString(), any());
+
+    Response<EnabledConnectionsPage> enabledConnectionsPageResponse = mock(Response.class);
+    doReturn(enabledConnectionsPageResponse).when(enabledConnectionsPage).execute();
+
+    EnabledConnectionsPage connectionsPage = new EnabledConnectionsPage(List.of());
+    doReturn(connectionsPage).when(enabledConnectionsPageResponse).getBody();
+
+    String createdBy = "system";
+    Organization result =
+        organizationService.updateStatus(
+            UUID.randomUUID().toString(), OrgStatusEnum.INACTIVE, createdBy);
+    OrganizationMetadata resultMetadata =
+        JsonToolkit.fromJson(JsonToolkit.toJson(result.getMetadata()), OrganizationMetadata.class);
+    assertEquals(OrgStatusEnum.INACTIVE, resultMetadata.getStatus());
+  }
+
+  @Test
   void testUpdateStatusActive() throws Auth0Exception {
     ManagementAPI managementAPI = mock(ManagementAPI.class);
     doReturn(managementAPI).when(auth0Client).getMgmtClient();
@@ -301,6 +368,90 @@ class OrganizationServiceTest {
     doReturn(connectionResponse).when(connectionsPageRequest).execute();
 
     ConnectionsPage existConnection = new ConnectionsPage(List.of(new Connection("test", "auth0")));
+    doReturn(existConnection).when(connectionResponse).getBody();
+
+    String createdBy = "system";
+    Organization result =
+        organizationService.updateStatus(
+            UUID.randomUUID().toString(), OrgStatusEnum.ACTIVE, createdBy);
+    OrganizationMetadata resultMetadata =
+        JsonToolkit.fromJson(JsonToolkit.toJson(result.getMetadata()), OrganizationMetadata.class);
+    assertEquals(OrgStatusEnum.ACTIVE, resultMetadata.getStatus());
+  }
+
+  @Test
+  void testUpdateStatusActiveBranch() throws Auth0Exception {
+    ManagementAPI managementAPI = mock(ManagementAPI.class);
+    doReturn(managementAPI).when(auth0Client).getMgmtClient();
+
+    OrganizationsEntity organizationsEntity = mock(OrganizationsEntity.class);
+    doReturn(organizationsEntity).when(managementAPI).organizations();
+
+    Request<Organization> queryRequest = mock(Request.class);
+    doReturn(queryRequest).when(organizationsEntity).get(anyString());
+
+    Response<Organization> queryResponse = mock(Response.class);
+    doReturn(queryResponse).when(queryRequest).execute();
+
+    Organization queryOrganization = new Organization("test");
+    queryOrganization.setDisplayName("test");
+    OrganizationMetadata metadata = new OrganizationMetadata();
+    metadata.setStatus(OrgStatusEnum.INACTIVE);
+    queryOrganization.setMetadata(
+        JsonToolkit.fromJson(JsonToolkit.toJson(metadata), new TypeReference<>() {}));
+    doReturn(queryOrganization).when(queryResponse).getBody();
+
+    Request<Organization> organizationRequest = mock(Request.class);
+    doReturn(organizationRequest).when(organizationsEntity).update(anyString(), any());
+    Response<Organization> updateResponse =
+        new Response<Organization>() {
+          @Override
+          public Map<String, String> getHeaders() {
+            return Map.of();
+          }
+
+          @Override
+          public Organization getBody() {
+            Organization updateOrganization = new Organization("test");
+            updateOrganization.setDisplayName("test");
+            OrganizationMetadata updateMetadata = new OrganizationMetadata();
+            updateMetadata.setStatus(OrgStatusEnum.ACTIVE);
+            updateOrganization.setMetadata(
+                JsonToolkit.fromJson(JsonToolkit.toJson(updateMetadata), new TypeReference<>() {}));
+            return updateOrganization;
+          }
+
+          @Override
+          public int getStatusCode() {
+            return HttpStatus.OK.value();
+          }
+        };
+    doReturn(updateResponse).when(organizationRequest).execute();
+
+    Request<EnabledConnectionsPage> enabledConnectionsPage = mock(Request.class);
+    doReturn(enabledConnectionsPage).when(organizationsEntity).getConnections(anyString(), any());
+
+    Response<EnabledConnectionsPage> enabledConnectionsPageResponse = mock(Response.class);
+    doReturn(enabledConnectionsPageResponse).when(enabledConnectionsPage).execute();
+
+    EnabledConnection enabledConnection = new EnabledConnection("test");
+    EnabledConnectionsPage connectionsPage = new EnabledConnectionsPage(List.of(enabledConnection));
+    doReturn(connectionsPage).when(enabledConnectionsPageResponse).getBody();
+
+    Request<Void> delRequest = mock(Request.class);
+    doReturn(delRequest).when(organizationsEntity).deleteConnection(anyString(), anyString());
+    doReturn(mock(Response.class)).when(delRequest).execute();
+
+    ConnectionsEntity connectionsEntity = mock(ConnectionsEntity.class);
+    doReturn(connectionsEntity).when(managementAPI).connections();
+
+    Request<ConnectionsPage> connectionsPageRequest = mock(Request.class);
+    doReturn(connectionsPageRequest).when(connectionsEntity).listAll(any());
+
+    Response<ConnectionsPage> connectionResponse = mock(Response.class);
+    doReturn(connectionResponse).when(connectionsPageRequest).execute();
+
+    ConnectionsPage existConnection = new ConnectionsPage(List.of());
     doReturn(existConnection).when(connectionResponse).getBody();
 
     String createdBy = "system";
