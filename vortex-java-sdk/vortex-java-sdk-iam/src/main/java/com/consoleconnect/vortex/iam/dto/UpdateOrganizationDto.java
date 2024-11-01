199cf4a8f8780c785c6ce5cf84e6defd373e4def
jackworkgit <hongzhang@consoleconnect.com>
2024-11-01 13:36:01 +0800

    add unit test

--- vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/UpdateOrganizationDto.java
+++ vortex-java-sdk/vortex-java-sdk-iam/src/main/java/com/consoleconnect/vortex/iam/dto/UpdateOrganizationDto.java
@@ -1,6 +1,5 @@
 package com.consoleconnect.vortex.iam.dto;
 
-import com.consoleconnect.vortex.iam.enums.OrgStatusEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -8,6 +7,4 @@ import lombok.Data;
 public class UpdateOrganizationDto {
   @JsonProperty("display_name")
   private String displayName;
-
-  private OrgStatusEnum status;
 }
