49e5e56d4e2aada2caee8fb91c3d1ad023fadf02
xiang <575059520@qq.com>
2024-11-05 19:13:54 +0800

    fix(sdk): decode and encode response

--- vortex-java-sdk/vortex-java-sdk-gateway/src/main/java/com/consoleconnect/vortex/gateway/filter/ResponseAdapterGatewayFilterFactory.java
+++ vortex-java-sdk/vortex-java-sdk-gateway/src/main/java/com/consoleconnect/vortex/gateway/filter/ResponseAdapterGatewayFilterFactory.java
@@ -1,18 +1,27 @@
 package com.consoleconnect.vortex.gateway.filter;
 
+import static java.util.function.Function.identity;
+
 import com.consoleconnect.vortex.gateway.adapter.RouteAdapter;
 import com.consoleconnect.vortex.gateway.adapter.RouteAdapterFactory;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.NettyWriteResponseFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyDecoder;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyEncoder;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
@@ -26,10 +35,21 @@ public class ResponseAdapterGatewayFilterFactory
     extends AbstractGatewayFilterFactory<AbstractGatewayFilterFactory.NameConfig> {
 
   private final RouteAdapterFactory adapterFactory;
+  private final Map<String, MessageBodyDecoder> messageBodyDecoders;
+  private final Map<String, MessageBodyEncoder> messageBodyEncoders;
 
-  public ResponseAdapterGatewayFilterFactory(RouteAdapterFactory adapterFactory) {
+  public ResponseAdapterGatewayFilterFactory(
+      RouteAdapterFactory adapterFactory,
+      Set<MessageBodyDecoder> messageBodyDecoders,
+      Set<MessageBodyEncoder> messageBodyEncoders) {
     super(NameConfig.class);
     this.adapterFactory = adapterFactory;
+    this.messageBodyDecoders =
+        messageBodyDecoders.stream()
+            .collect(Collectors.toMap(MessageBodyDecoder::encodingType, identity()));
+    this.messageBodyEncoders =
+        messageBodyEncoders.stream()
+            .collect(Collectors.toMap(MessageBodyEncoder::encodingType, identity()));
   }
 
   @Override
@@ -71,31 +91,56 @@ public class ResponseAdapterGatewayFilterFactory
                                 .map(
                                     // Step 2: read response body
                                     dataBuffers -> {
-                                      ByteArrayOutputStream outputStream =
-                                          new ByteArrayOutputStream();
-                                      dataBuffers.stream()
-                                          .forEach(
-                                              buffer -> {
-                                                byte[] bytes = new byte[buffer.readableByteCount()];
-                                                buffer.read(bytes);
-                                                DataBufferUtils.release(buffer);
-                                                try {
-                                                  outputStream.write(bytes);
-                                                } catch (IOException e) {
-                                                  log.info("response body read bytes failed", e);
-                                                }
-                                              });
+                                      DataBufferFactory bufFactory = new DefaultDataBufferFactory();
+                                      DataBuffer buffer = bufFactory.join(dataBuffers);
 
-                                      byte[] originalBody = outputStream.toByteArray();
-                                      // Step 3: process the response body with the adapter
-                                      byte[] resBody = adapter.process(exchange, originalBody);
-                                      return bufferFactory().wrap(resBody);
+                                      byte[] content = new byte[buffer.readableByteCount()];
+                                      buffer.read(content);
+                                      DataBufferUtils.release(buffer);
+
+                                      // Step 3: decode
+                                      content = extractBody(exchange, content);
+
+                                      // Step 4: process the response body with the adapter
+                                      byte[] resBody = adapter.process(exchange, content);
+
+                                      // Step 5: encode
+                                      return bufferFactory().wrap(writeBody(exchange, resBody));
                                     }));
                       }
                       return super.writeWith(body);
                     }
                   })
               .build());
+    }
+
+    private byte[] extractBody(ServerWebExchange exchange, byte[] resBytes) {
+
+      List<String> encodingHeaders =
+          exchange.getResponse().getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
+      for (String encoding : encodingHeaders) {
+        MessageBodyDecoder decoder = messageBodyDecoders.get(encoding);
+        log.info("extractBody encoding: {}, decoder{}", encoding, decoder.getClass());
+        if (decoder != null) {
+          return decoder.decode(resBytes);
+        }
+      }
+      return resBytes;
+    }
+
+    private byte[] writeBody(ServerWebExchange exchange, byte[] resBytes) {
+      List<String> encodingHeaders =
+          exchange.getResponse().getHeaders().getOrEmpty(HttpHeaders.CONTENT_ENCODING);
+      for (String encoding : encodingHeaders) {
+        MessageBodyEncoder encoder = messageBodyEncoders.get(encoding);
+        if (encoder != null) {
+          log.info("extractBody encoding: {}, encoder{}", encoding, encoder.getClass());
+          DataBufferFactory bufferFactory = exchange.getResponse().bufferFactory();
+          return encoder.encode(bufferFactory.wrap(resBytes));
+        }
+      }
+
+      return resBytes;
     }
 
     @Override
