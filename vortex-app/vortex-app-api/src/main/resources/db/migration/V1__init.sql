f63e53f8268417d61a689d70b953976cb3b23872
xiang <575059520@qq.com>
2024-11-20 17:49:11 +0800

    fix table column resource_type

---
+++ vortex-app/vortex-app-api/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+ALTER TABLE vortex_order DROP COLUMN resource_type;
+
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='vortex_order' and column_name='resource_type')
+  THEN
+    ALTER TABLE vortex_order DROP CONSTRAINT IF EXISTS vortex_order_resource_type_check;
+    ALTER TABLE vortex_order ADD CONSTRAINT vortex_order_resource_type_check
+     CHECK (((resource_type)::text = ANY ((ARRAY['PORT'::character varying, 'L2'::character varying, 'L3'::character varying])::text[])));
+  END IF;
+
+END $$;
