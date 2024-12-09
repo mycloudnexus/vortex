01619d040047325b80cbe4fe7c19607e30badfc5
li yang <lyang@pccwglobal.com>
2024-12-09 15:42:04 +0800

    feat: add test

--- vortex-app/vortex-app-portal/src/utils/helpers/__test__/request.test.tsx
+++ vortex-app/vortex-app-portal/src/utils/helpers/__test__/request.test.tsx
@@ -3,6 +3,13 @@ import request from '@/utils/helpers/request'
 import { storeOrg, getOrg } from '@/utils/helpers/token'
 
 describe('Axios interceptor', () => {
+  const url = 'https://www.test.com'
+  Object.defineProperty(window, 'location', {
+    value: {
+      origin: url
+    },
+    writable: true
+  })
   let mock: any
 
   beforeEach(() => {
@@ -25,6 +32,15 @@ describe('Axios interceptor', () => {
     expect(mock.history.get[0].headers['Authorization']).toEqual(`Bearer ${testToken}`)
     expect(mock.history.get[0].headers['x-vortex-customer-org-id']).toEqual(testCompany)
   })
+  it('should not have  header to request', async () => {
+    window.localStorage.removeItem('token')
+    window.localStorage.removeItem('currentCompany')
+    mock.onGet('/users').reply(200, [])
+    await request.get('/users')
+
+    expect(mock.history.get[0].headers['Authorization']).toBeFalsy()
+    expect(mock.history.get[0].headers['x-vortex-customer-org-id']).toBeFalsy()
+  })
 
   it('should handle response errors', async () => {
     mock.onGet('/users').reply(500, 'Internal Server Error')
@@ -39,20 +55,22 @@ describe('Axios interceptor', () => {
 
   it('should redirect to login when response code is 401', async () => {
     storeOrg('org')
-    const url = 'https://www.test.com'
-    Object.defineProperty(window, 'location', {
-      value: {
-        origin: url
-      },
-      writable: true
-    })
     mock.onGet('/users').reply(401, { error: { message: 'The session token been deleted' } })
     try {
       await request.get('/users')
     } catch (error: any) {
       expect(error.response.status).toEqual(401)
-      console.log('---getOrg()getOrg()', getOrg())
       expect(window.location.href).toContain(`/${getOrg()}/login`)
+    }
+  })
+  it('it should redirect to login when response code is 401 (reseller)', async () => {
+    window.localStorage.removeItem('org')
+    mock.onGet('/users').reply(401, { error: { message: 'The user is not logged in' } })
+    try {
+      await request.get('/users')
+    } catch (error: any) {
+      expect(error.response.status).toEqual(401)
+      expect(window.location.href).toContain(`/login`)
     }
   })
 })
