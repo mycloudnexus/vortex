2b5cb6f7da6b05c6317deea7fc378c92794da872
li yang <lyang@pccwglobal.com>
2024-12-12 14:14:14 +0800

    merge main

--- vortex-app/vortex-app-portal/src/pages/Modules/ModulePortContainer/index.tsx
+++ vortex-app/vortex-app-portal/src/pages/Modules/ModulePortContainer/index.tsx
@@ -6,10 +6,12 @@ import { Suspense } from 'react'
 const ModulePort = lazyLoadFederatedModule('portal_client_port_module')
 
 const ModulePortContainer = (props: any): JSX.Element => {
-  const { user, roleList, mainColor } = useAppStore()
+  const { downstreamUser, roleList, mainColor } = useAppStore()
   return (
     <Suspense fallback={<Spin />}>
-      {ModulePort && <ModulePort baseUrl='ports' user={user} roleList={roleList} mainColor={mainColor} {...props} />}
+      {ModulePort && (
+        <ModulePort baseUrl='ports' user={downstreamUser} roleList={roleList} mainColor={mainColor} {...props} />
+      )}
     </Suspense>
   )
 }
