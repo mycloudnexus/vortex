2fee047264a8f988c950d61abae9795ec1e8017b
pccw-reguita <reguita@consoleconnect.com>
2024-12-12 14:41:12 +0800

    fix: remove try catch block

--- vortex-app/vortex-app-portal/src/services/index.ts
+++ vortex-app/vortex-app-portal/src/services/index.ts
@@ -11,59 +11,34 @@ import {
 } from './types'
 
 export const getCompanyList = async (): Promise<RequestResponse<IOrganization>> => {
-  try {
-    const response = await request(ORGANIZATIONS, {
-      params: {
-        size: -1
-      }
-    })
-    return response?.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request(ORGANIZATIONS, {
+    params: {
+      size: -1
+    }
+  })
+  return response?.data
 }
 
 export const createOrganization = async (req: CreateOrganizationRequestBody): Promise<CreateOrganizationResponse> => {
-  try {
-    const response = await request.post(ORGANIZATIONS, req)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.post(ORGANIZATIONS, req)
+  return response.data
 }
 
 export const updateOrganization = async (req: UpdateOrganizationRequestBody): Promise<CreateOrganizationResponse> => {
   const { id, request_body } = req
-  try {
-    const response = await request.patch(`${ORGANIZATIONS}/${id}`, request_body)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.patch(`${ORGANIZATIONS}/${id}`, request_body)
+  return response.data
 }
 
 export const getOrganizationById = async (orgId: string): Promise<CreateOrganizationResponse> => {
-  try {
-    const response = await request(`${ORGANIZATIONS}/${orgId}`)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request(`${ORGANIZATIONS}/${orgId}`)
+  return response.data
 }
 
 export const createConnection = async (
   orgId: string,
   req: AddConnectionRequestBody
 ): Promise<RequestResponse<AddConnectionResponse>> => {
-  try {
-    const response = await request.post(`${ORGANIZATIONS}/${orgId}/connection`, req)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.post(`${ORGANIZATIONS}/${orgId}/connection`, req)
+  return response.data
 }
